f2e047a5c31b319109cb79240db03c9b062695c8
Parul Saini <sainiparul700@gmail.com>
2023-10-11 23:08:28 +0530

    -registered page and add icon -57:46

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,25 +1,72 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styled from 'styled-components';
 import {Link} from "react-router-dom";
 import logo from "../assests/logo.png"
-import { Row, Col} from 'react-bootstrap';
+import { Row, Col, Toast} from 'react-bootstrap';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
-
+import { ToastContainer, toast } from 'react-toastify';
+import "react-toastify/dist/ReactToastify.css";
 
 function Register() {
-  const  handleEvent=(event)=>{
-  event.preventDefault();
-  alert("form submit");
-  }
+  const [values ,setValues]= useState({
+  userName:"",
+  email:"",
+  password:"",
+  confirmPassword:""
+  });
 
   const  handleChange=(event)=>{
-  
+    setValues({...values, [event.target.name]:[event.target.value]});
   }
+
+  const handleEvent=(event)=>{
+    event.preventDefault();
+    if(handleValidation() === true) {
+      alert("form submit");
+    }
+  }
+
+  // CSS of toast 
+  const toastCSS = {
+    position:"top-right",
+    autoClose: 4000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+    theme:"light"
+  }
+
+  // check the input fields before submitting the form 
+  const handleValidation=()=>{
+   const {userName,email,password,confirmPassword} = values;
+
+   if(email.toString().length < 1){
+    toast.error("All fields are Required ", toastCSS);
+    return false;
+   }
+   else if(password.toString() !== confirmPassword.toString() ){
+    toast.error("Password and Confirm Password should be same ", toastCSS);
+    return false;
+   }
+   else if(password.toString().length < 5 ){
+    toast.error("Password should be equal or greater than 5 characters", toastCSS);
+    return false;
+   }
+   else if(userName.toString().length <= 3){
+    toast.error("User-Name should be greater than 3 characters", toastCSS);
+    return false;
+   }
+   return true;
+  }
+
 
   return (
     <>
       <div className="container-register" style={{}}>
+      
         <Row>
           <Col lg="5" s="12">
             <div className="brandName" style={{
@@ -37,32 +84,34 @@ function Register() {
           
           <Form onSubmit={(event)=>{handleEvent(event)}}>
             <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-              <Form.Label>User Name</Form.Label>
-              <Form.Control type="text" placeholder="User name" name='user name' onChange={(e)=>{handleChange(e)}}/>
+              <Form.Label>User Name <sup>*</sup></Form.Label>
+              <Form.Control type="text" placeholder="User name" value={values.userName} name='userName' onChange={(e)=>{handleChange(e)}}/>
             </Form.Group>
             <Form.Group className="mb-3" controlId="formBasicEmail">
-              <Form.Label>Email address</Form.Label>
-              <Form.Control type="email" placeholder="Enter email"  name='email' onChange={(e)=>{handleChange(e)}}/>
+              <Form.Label>Email address <sup>*</sup></Form.Label>
+              <Form.Control type="email" placeholder="Enter email" value={values.email}  name='email' onChange={(e)=>{handleChange(e)}}/>
             </Form.Group>
 
             <Form.Group className="mb-3" controlId="formBasicPassword">
-              <Form.Label>Password</Form.Label>
-              <Form.Control type="password" placeholder="Password"  name='password'  onChange={(e)=>{handleChange(e)}}/>
+              <Form.Label>Password <sup>*</sup></Form.Label>
+              <Form.Control type="password" placeholder="Password" value={values.password} name='password'  onChange={(e)=>{handleChange(e)}}/>
             </Form.Group>
 
             <Form.Group className="mb-3" controlId="formBasicPassword">
-              <Form.Label>Confirm Password</Form.Label>
-              <Form.Control type="password" placeholder="Confirm Password"  name='ConfirmPassword' onChange={(e)=>{handleChange(e)}}/>
+              <Form.Label>Confirm Password <sup>*</sup></Form.Label>
+              <Form.Control type="password" placeholder="Confirm Password" value={values.confirmPassword}  name='confirmPassword' onChange={(e)=>{handleChange(e)}}/>
             </Form.Group>
     
             <Button  style={{ backgroundColor: '#017dc0', color: 'white' }} variant="primary" type="submit">
               Create User  
             </Button>
-            <p>  Already have an account ? <Link to="/login">Login</Link> </p>
+            <p className='m-2'>  Already have an account ? <Link to="/login">Login</Link> </p>
           </Form>
           
           </Col>
         </Row>
+        <ToastContainer />
+       
       </div>
     </>
   )
